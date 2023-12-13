d7af8b762cc274cd4ec6bf4bfd3c33e8ceef6227
Lotso <lotso@localhost.localdomain>
2023-12-13 10:26:31 +0700

    First Commit

---
+++ main.py
@@ -0,0 +1,7 @@
+p = 5
+l = 2
+luas_persegi_panjang = p * l
+keliling_persegi_panjang = 2 * (p + l)
+
+print(f'Luas dari panjang {p} dan lebar {l} adalah {luas_persegi_panjang}')
+print(f'Keliling dari panjang {p} dan lebar {l} adalah {keliling_persegi_panjang}')
